3faaaa7bdff723be451352a56a4add7d677719a1
ahmedfudge <afaraj@outlook.com>
2023-07-14 08:17:57 -0700

    redirect to root if no token

--- client/src/pages/root.tsx
+++ client/src/pages/root.tsx
@@ -4,16 +4,23 @@ import Dashboard from "./Dashboard";
 import {
     createBrowserRouter,
     RouterProvider,
+    useNavigate,
 } from "react-router-dom";
 
 import { User } from '../models';
 import OrderScreenApp from "./OrderScreenApp";
 
+
 const queryParams = new URLSearchParams(window.location.search);
 console.log(queryParams.get("access_token"));
 
-const token = queryParams.get("access_token") || "";
+const token_query_param = queryParams.get("access_token");
 queryParams.delete("access_token");
+
+if (token_query_param == null) {
+    useNavigate()('/');
+}
+const token = token_query_param || "";
 
 const router = createBrowserRouter([
     {
