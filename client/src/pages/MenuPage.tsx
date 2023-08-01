444a868780022aa2d9e1b335f9315e4682d0365d
ahmedfudge <afaraj@outlook.com>
2023-08-01 14:26:26 -0700

    storing access tokens to localstorage

--- client/src/pages/MenuPage.tsx
+++ client/src/pages/MenuPage.tsx
@@ -13,23 +13,29 @@ const MenuPage = ({ token }: MenuProps) => {
   const [menu, setMenu] = useState<Menu>([]);
 
   useEffect(() => {
+    console.log('token', token);
+    console.log('localStorage', localStorage.getItem("accessToken"));
     getMenu(token).then((m) => {
       setMenu(m);
     });
   }, []);
 
   const isItem = (item: MenuItem): item is Item => item.type === 'ITEM';
+
+  const renderItem = (item: MenuItem) => {
+    switch (item.type) {
+      case 'ITEM':
+        return (<MenuItemCard item={item} />);
+      case 'MODIFIER_LIST':
+        return (<div>Modifier list</div>);
+    }
+  }
   
   return (
     <div className='App-header'>
-      <div>
+      <div> 
         <h2>Menu page</h2>
-        {menu.filter(isItem).map((item) => {
-          console.log(item);
-          return (
-            <MenuItemCard item={item as Item} />
-          )
-        }) }
+        { menu.map(renderItem) }
       </div>
     </div>
   );
